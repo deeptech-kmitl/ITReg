1fb27804be6ef68528ead077c4fd446e41d1e31c
MindNara <88419813+MindNara@users.noreply.github.com>
2024-01-07 18:36:07 +0700

    Add bg in sign in / sign up page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SignInBG from '../assets/SignInBG.png'
+
 
 function SignIn() {
     return (
-        <div>SignIn</div>
+        <div style={{ backgroundImage: `url(${SignInBG})` }}
+            className='bg-no-repeat bg-cover bg-center h-screen px-40 py-16 bg-[#181754] text-white'
+        >
+            <div className='flex justify-center items-center h-full text-3xl'>
+                Sign In
+            </div>
+        </div>
     )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import SignUpBG from '../assets/SignUpBG.png'
 
 function SignUp() {
     return (
-        <div>SignUp</div>
+        <div style={{ backgroundImage: `url(${SignUpBG})` }}
+            className='bg-no-repeat bg-cover bg-center h-screen px-40 py-16 bg-[#181754] text-white'
+        >
+            <div className='flex justify-center items-center h-full text-3xl'>
+                Sign Up
+            </div>
+        </div>
     )
 }
 
