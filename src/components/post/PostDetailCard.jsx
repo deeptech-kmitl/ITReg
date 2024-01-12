345cb93634a1f188fda96fc93245fbaeebd27cbc
Punimmiki <89025623+Punimmiki@users.noreply.github.com>
2024-01-12 19:29:29 +0700

    Add hearts and comments / The heart changes color when pressed.

--- src/components/post/PostDetailCard.jsx
+++ src/components/post/PostDetailCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
 
 const DetailCard = [
   {
@@ -8,7 +9,10 @@ const DetailCard = [
     time: "5.20 PM",
     message:
       "คณะเทคโนโลยีสารสนเทศ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง เปิดรับสมัครเข้าศึกษาต่อระดับปริญญาตรี ประจำปีการศึกษา 2563 TCAS 63 รอบที่ 4 (Admission) 2 หลักสูตร 1) เทคโนโลยีสารสนเทศ (Information Technology : IT) รับจำนวน 15 คน 2) ข้อมูลและการวิเคราะห์เชิงธุรกิจ (Data Science and Business Analytics : DSBA) รับจำนวน 6 คน ยกเลิกการสอบสัมภาษณ์ เนื่องจากสถานการณ์เชื้อ COVID -19 ระบาด",
-    image: "https://www.it.kmitl.ac.th/wp-content/uploads/2020/05/1300x867pix-%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%A1%E0%B8%B1%E0%B8%84%E0%B8%A3-TCAS63-4-Admission.jpg",
+    image:
+      "https://www.it.kmitl.ac.th/wp-content/uploads/2020/05/1300x867pix-%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%A1%E0%B8%B1%E0%B8%84%E0%B8%A3-TCAS63-4-Admission.jpg",
+    like: 145,
+    comment: 76,
   },
   {
     titlename: "ชำระค่าเทอม",
@@ -16,12 +20,26 @@ const DetailCard = [
     date: "18/12/2023",
     time: "6.30 AM",
     message:
-      "ขั้นตอนการพิมพ์ใบชำระค่าธรรรมเนียมการศึกษา นักศึกษาทุกชั้นปี (ป.ตรี/โท/เอก) สามารถพิมพ์ใบแจ้งชําระเงินจากระบบ แล้วนําไปยื่นชําระเงินผ่านเคาท์เตอร์ธนาคาร (หรือในนักศึกษาระดับป.ตรี สแกนบาร์โค้ดผ่านแอพธนาคาร) โดยไม่มีค่าปรับชำระเงินล่าช้า จนถึงวันศุกร์ที่ 7 พฤษภาคม 2564",
+      "ขั้นตอนการพิมพ์ใบชำระค่าธรรมเนียมการศึกษา นักศึกษาทุกชั้นปี (ป.ตรี/โท/เอก) สามารถพิมพ์ใบแจ้งชําระเงินจากระบบ แล้วนําไปยื่นชําระเงินผ่านเคาท์เตอร์ธนาคาร (หรือในนักศึกษาระดับป.ตรี สแกนบาร์โค้ดผ่านแอพธนาคาร) โดยไม่มีค่าปรับชำระเงินล่าช้า จนถึงวันศุกร์ที่ 7 พฤษภาคม 2564",
     image: "",
+    like: 235,
+    comment: 91,
   },
 ];
 
 const PostDetailCard = () => {
+  const [likedPosts, setLikedPosts] = useState([]);
+
+  const handleLikeClick = (index) => {
+    if (likedPosts.includes(index)) {
+      // Unlike post
+      setLikedPosts(likedPosts.filter((i) => i !== index));
+    } else {
+      // Like post
+      setLikedPosts([...likedPosts, index]);
+    }
+  };
+
   return (
     <div className="mt-5">
       {DetailCard.map((detail, index) => (
@@ -44,7 +62,7 @@ const PostDetailCard = () => {
             </div>
             <div className="mt-5">
               <p className="text-black text-l font-light">{detail.message}</p>
-              {/* เช็คว่ามีรูปภาพหรือไม่ ถ้ามีให้แสดง */}
+              {/* เช็คว่ามีรูปภาพป่าว ถ้ามีให้แสดง */}
               {detail.image && (
                 <img
                   src={detail.image}
@@ -52,6 +70,24 @@ const PostDetailCard = () => {
                   className="mt-4 w-full rounded-[30px] border-[1px] border-solid border-gray-300"
                 />
               )}
+            </div>
+            <div className="mt-3 flex items-start">
+              <Icon
+                icon="bxs:heart"
+                color={likedPosts.includes(index) ? "#d91818" : "#151c38"}
+                width="22"
+                height="22"
+                onClick={() => handleLikeClick(index)}
+              />
+              <div className="ml-1 mt-[1px]">
+                <p className="text-[#151C38] text-sm mr-3">{detail.like}</p>
+              </div>
+              <div className="mt-[2.5px]">
+                <Icon icon="fa:comment-o" color="#151c38" width="19" height="19" />
+              </div>
+              <div className="ml-1 mt-[1px]">
+                <p className="text-[#151C38] text-sm">{detail.comment}</p>
+              </div>
             </div>
           </div>
         </div>
