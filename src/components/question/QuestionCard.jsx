ad041244300b5d3288e2038d874e5f4ff9e1e973
Urangotan-pure <orangutan11112000@gmail.com>
2024-01-13 13:05:36 +0700

    add condition editQuestion and likeCardQuestion

--- src/components/question/QuestionCard.jsx
+++ src/components/question/QuestionCard.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 
 function QuestionCard() {
 
-    const database = [
+    const user = "Anonymous1";
+
+    const [database, setDatabase] = useState([
         {
             name: "Anonymous1",
             date: "11/1/2567",
@@ -18,7 +20,7 @@ function QuestionCard() {
             date: "14/1/2567",
             time: "15.30 PM",
             details: "อาจารย์กดเกรดไหม",
-            like: ["Anonymous1", "Anonymous6", "Anonymous8"],
+            like: ["Anonymous6", "Anonymous8"],
             dislike: ["Anonymous5", "Anonymous11", "Anonymous12"],
             answer: ["Anonymous7", "Anonymous15", "Anonymous18", "Anonymous8", "Anonymous6", "Anonymous7"],
         },
@@ -36,17 +38,42 @@ function QuestionCard() {
             date: "14/1/2567",
             time: "15.30 PM",
             details: "อาจารย์กดเกรดไหม",
-            like: ["Anonymous1", "Anonymous6", "Anonymous8"],
+            like: ["Anonymous19", "Anonymous6", "Anonymous8"],
             dislike: ["Anonymous5", "Anonymous11", "Anonymous12"],
             answer: ["Anonymous7", "Anonymous15", "Anonymous18", "Anonymous8", "Anonymous6", "Anonymous7"],
         },
-    ];
+    ]);
+
+    const handleToggleLike = (index) => {
+        console.log(index)
+        setDatabase(
+            (dumyDatabase) => {
+                const updatedDatabase = [...dumyDatabase];
+                const likeByUser = updatedDatabase[index].like.includes(user);
+
+                if (likeByUser) {
+                    // ถ้า user กดถูกใจในข้อมูลเดิม ให้ลบออก
+                    updatedDatabase[index].like = updatedDatabase[index].like.filter(user_id => user_id !== user);
+                } else {
+                    // ถ้า user ไม่ได้กดถูกใจในข้อมูลเดิม ให้เพิ่มเข้า
+                    updatedDatabase[index].like.push(user);
+                }
+                return updatedDatabase;
+            }
+        )
+    }
 
     return (
         <div className="overflow-y-scroll static h-[38dvh]">
             {database.map((question, index) => (
-                <div className="pt-[20px]">
-                    <div className="w-[100%] border-2 rounded-[10px] bg-[#ffffff] p-[20px]">
+                <div key={index} className="pt-[20px]">
+                    <div className="w-[100%] border-2 rounded-[10px] bg-[#ffffff] p-[20px] relative ">
+                        {/* Edit/Delete Question */}
+                        {/* Can it be Edit/Delete ? */}
+                        {question.name === user &&
+                            <div className="mt-[2.5px] absolute right-5">
+                                <Icon icon="prime:ellipsis-h" color="#151c38" width="19" height="19" />
+                            </div>}
                         {/* profile */}
                         <div className="mt-3 flex items-start">
                             <div className="w-[50px] h-[50px] flex-shrink-0 rounded-full bg-[#151C38]"></div>
@@ -62,14 +89,28 @@ function QuestionCard() {
                             <div className="text-black text-l font-light">{question.details}</div>
                             {/* emotion */}
                             <div className="mt-3 flex items-start">
-                                <div name="like" className="rotate-0">
-                                    <Icon
-                                        icon="streamline:like-1"
-                                        color="#151c38"
-                                        width="22"
-                                        height="22"
-                                    />
-                                </div>
+                                {/* Did you like it ? */}
+                                {question.like.filter(user_id => user_id === user).length > 0 ?
+                                    (
+                                        <div name="like" className="rotate-0" onClick={() => handleToggleLike(index)}>
+                                            <Icon
+                                                icon="streamline:like-1-solid"
+                                                color="#151c38"
+                                                width="22"
+                                                height="22"
+                                            />
+                                        </div>
+                                    ) : (
+                                        <div name="like" className="rotate-0" onClick={() => handleToggleLike(index)}>
+                                            <Icon
+                                                icon="streamline:like-1"
+                                                color="#151c38"
+                                                width="22"
+                                                height="22"
+                                            />
+                                        </div>
+                                    )}
+
                                 <div className="ml-1 mt-[1px]">
                                     <p className="text-[#151C38] text-sm mr-3">{question.like.length}</p>
                                 </div>
