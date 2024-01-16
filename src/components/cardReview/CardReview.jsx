c06c29cca990af84c6d896f7b1f2c12ce301290f
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2024-01-16 17:58:50 +0700

    add createReview popup in Review page

--- src/components/cardReview/CardReview.jsx
+++ src/components/cardReview/CardReview.jsx
@@ -89,7 +89,7 @@ function CardReview() {
     const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
 
     const toggleModalDelete = () => {
-        setIsModalDeleteOpen(!isModalEditOpen);
+        setIsModalDeleteOpen(!isModalDeleteOpen);
     };
 
     return (
@@ -164,7 +164,7 @@ function CardReview() {
                                                     Edit Review
                                                 </h5>
                                                 {/* close */}
-                                                <button type="button" class="absolute top-5 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal" onClick={() => setIsModalEditOpen(false)}>
+                                                <button type="button" class="absolute top-5 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => setIsModalEditOpen(false)}>
                                                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                                     </svg>
@@ -229,6 +229,7 @@ function CardReview() {
                             </div>
                         )}
 
+                        {/* modal delete */}
                         {isModalDeleteOpen && (
                             <div
                                 id="modal-delete"
@@ -261,7 +262,7 @@ function CardReview() {
                                                     Delete Review
                                                 </h5>
                                                 {/* close */}
-                                                <button type="button" class="absolute top-5 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal" onClick={() => setIsModalDeleteOpen(false)}>
+                                                <button type="button" class="absolute top-5 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => setIsModalDeleteOpen(false)}>
                                                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                                     </svg>
--- src/pages/ReviewSubjectDetail.jsx
+++ src/pages/ReviewSubjectDetail.jsx
@@ -6,6 +6,14 @@ import QuestionCard from '../components/question/QuestionCard'
 function ReviewSubjectDetail() {
 
   const [activeTab, setActiveTab] = useState("review")
+  
+  // Modal create open
+  const [isModalCreateOpen, setIsModalCreateOpen] = useState(false);
+
+  const toggleModalCreate = () => {
+    setIsModalCreateOpen(!isModalCreateOpen);
+  };
+  console.log(isModalCreateOpen)
 
   return (
     <div className='w-full'>
@@ -40,12 +48,109 @@ function ReviewSubjectDetail() {
                       className="py-[6px] px-[12px] rounded-[10px] bg-gradient-to-br 
                     from-[#0D0B5F] from-[12.5%] to-[#029BE0] to-[100%] text-[#ffffff]  
                     hover:from-[#029BE0] hover:to-[#0D0B5F]
-                    absolute right-2 mt-2 text-[16px]"
+                    absolute right-2 mt-[6px] text-[16px] w-[40px] h-[40px]" onClick={toggleModalCreate}
                     >
-                      <img src='https://img.icons8.com/material-outlined/24/FFFFFF/plus-math--v1.png'></img>
+                      <img src='https://img.icons8.com/ios-filled/20/FFFFFF/plus-math.png'></img>
                     </button>
                   </div>
                   <CardReview />
+                  {/* Modal create Review */}
+                  {isModalCreateOpen && (
+                    <div
+                      id="modal-create"
+                      tabIndex="-1"
+                      aria-hidden="true"
+                      className="fixed inset-0 overflow-y-auto"
+                      style={{ zIndex: 1001, borderRadius: "30px" }}
+                    // onClick={() => setIsModalOpen(false)}
+                    >
+                      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <div
+                          className="fixed inset-0 transition-opacity"
+                          aria-hidden="true"
+                        >
+                          <div className="absolute inset-0 bg-gray-500 opacity-25"></div>
+                        </div>
+
+                        <span
+                          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                          aria-hidden="true"
+                        >
+                          &#8203;
+                        </span>
+                        <div className="inline-block align-bottom bg-white rounded-[20px] text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                          <div className="bg-white rounded-[30px]">
+
+                            {/* header */}
+                            <div className="flex items-center justify-between p-4 border-b border-gray-200">
+                              <h5 className="text-[27px] font-semibold bg-gradient-to-br from-[#0D0B5F] from-[12.5%] to-[#029BE0] to-[100%] text-transparent bg-clip-text text-center w-full">
+                                Create Review
+                              </h5>
+                              {/* close */}
+                              <button type="button" class="absolute top-5 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => setIsModalCreateOpen(false)}>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                              </button>
+                            </div>
+                            {/* body */}
+                            <div className="p-4 md:p-5 space-y-4">
+                              <textarea
+                                rows="4"
+                                cols="50"
+                                placeholder="Text something review ..."
+                                className="border-none outline-none p-2 mb-4 w-full resize-none focus:ring-0 text-base font-normal"
+                              // value="eiei"
+                              />
+                            </div>
+                            <div className="flex flex-row">
+                              <div className="w-full px-3 mb-6">
+                                <label className="text-[16px] max-2xl:text-[16px] font-medium mb-2 text-gray-500" htmlFor="year">
+                                  Rating
+                                </label>
+                                <select
+                                  className='bg-[#F4F4F4] border border-gray-200 rounded-[10px] text-gray-500 mt-2 text-[16px] max-2xl:text-[15px] w-full py-2 px-3 leading-tight focus:outline-none focus:border-gray-500'
+                                  name="selectedPoint">
+                                  <option value="point1">1 point</option>
+                                  <option value="point2">2 point</option>
+                                  <option value="point3">3 point</option>
+                                  <option value="point4">4 point</option>
+                                  <option value="point5">5 point</option>
+
+                                </select>
+                              </div>
+                              <div className="w-full px-3 mb-6">
+                                <label className="text-[16px] max-2xl:text-[16px] font-medium mb-2 text-gray-500" htmlFor="year">
+                                  Grade
+                                </label>
+                                <select
+                                  className='bg-[#F4F4F4] border border-gray-200 text-gray-500 rounded-[10px] mt-2 text-[16px] max-2xl:text-[15px] w-full py-2 px-3 leading-tight focus:outline-none focus:border-gray-500'
+                                  name="selectedGrade">
+                                  <option value="GradeA">A</option>
+                                  <option value="GradeB+">B+</option>
+                                  <option value="GradeB">B</option>
+                                  <option value="GradeC+">C+</option>
+                                  <option value="GradeC">C</option>
+                                  <option value="GradeD+">D+</option>
+                                  <option value="GradeD">D</option>
+                                </select>
+                              </div>
+                            </div>
+                            {/* footer */}
+                            <div className="flex items-center p-4 md:p-5 rounded-b mt-[-20px] mb-2">
+                              <button
+                                onClick={() => setIsModalCreateOpen(false)}
+                                type="button"
+                                className="text-white bg-gradient-to-br from-[#0D0B5F] to-[#029BE0] hover:from-[#029BE0] hover:to-[#0D0B5F] font-medium rounded-lg text-lg px-10 py-2 text-center w-full"
+                              >
+                                Post
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>}
             {activeTab === "question" &&
